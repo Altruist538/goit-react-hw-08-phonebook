4de016bd5c17e5b3564324c5e2449bc6b976909b
Altruist538 <102470985+Altruist538@users.noreply.github.com>
2023-09-29 19:12:32 +0000

    Deploying to gh-pages from @ Altruist538/goit-react-hw-08-phonebook@8863e3c60017ac6d31a4b08fa8d44407d258fc4c 🚀

---
+++ static/js/266.208f059a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var i=n(184),c={container:{minHeight:"calc(40vh - 50px)",display:"flex",alignItems:"center",justifyContent:"center"},title:{fontWeight:500,fontSize:48,textAlign:"center"}};function r(){return(0,i.jsx)("div",{style:c.container,children:(0,i.jsx)("h1",{style:c.title,children:"Phone book welcome page"})})}}}]);
+//# sourceMappingURL=266.208f059a.chunk.js.map
